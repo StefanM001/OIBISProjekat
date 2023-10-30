b4acb39033696c2a3b9b06853003a408b9d72458
srdjan0101 <srdjan.mladjenovic.2001@gmail.com>
2023-10-30 18:07:07 +0100

    autentifikacija i sertifikati

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,9 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
+using System.Security.Principal;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Client
 {
@@ -14,12 +17,18 @@ namespace Client
         {
             string address = "net.tcp://localhost:4001/IChangeConfig";
             NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType =
+            TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel =
+            ProtectionLevel.EncryptAndSign;
 
             ChannelFactory<IChangeConfig> channel = new ChannelFactory<IChangeConfig>(binding, address);
 
             IChangeConfig proxy = channel.CreateChannel();
 
             Console.WriteLine("Connection with MS servis is succesfull.");
+            Console.WriteLine("Korisnik koji je pokrenuo klijenta je : " + WindowsIdentity.GetCurrent().Name);
             Console.WriteLine("//Login screen and implementation for changing autorization levels comes later.");
 
             Console.WriteLine(proxy.SendMessage());
--- IDS/Program.cs
+++ IDS/Program.cs
@@ -1,10 +1,15 @@
 ﻿using Common;
+using Manager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using Formatter = Manager.Formatter;
 
 namespace IDS
 {
@@ -12,10 +17,30 @@ namespace IDS
     {
         static void Main(string[] args)
         {
-            string address = "net.tcp://localhost:4000/IMalwareScanning";
+            
             NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            string srvCertCN = "wcfservice";
+
+            X509Certificate2 srvCert = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople, StoreLocation.LocalMachine, srvCertCN);
+            EndpointAddress address = new EndpointAddress(new Uri("net.tcp://localhost:4000/IMalwareScanning"),
+                                      new X509CertificateEndpointIdentity(srvCert));
 
             ChannelFactory<IMalwareScanning> channel = new ChannelFactory<IMalwareScanning>(binding, address);
+
+            string cltCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+
+            channel.Credentials.ServiceCertificate.Authentication.CertificateValidationMode = System.ServiceModel.Security.X509CertificateValidationMode.ChainTrust;
+            channel.Credentials.ServiceCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            /// Set appropriate client's certificate on the channel. Use CertManager class to obtain the certificate based on the "cltCertCN"
+            channel.Credentials.ClientCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, cltCertCN);
+
+            
+
+            /// Use CertManager class to obtain the certificate based on the "srvCertCN" representing the expected service identity.
+
 
             IMalwareScanning proxy = channel.CreateChannel();
 
