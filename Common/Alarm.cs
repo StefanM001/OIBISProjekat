68ac330918982d616be293564315fab80a541a8f
Stefan Malinović <malina.stefan39@gmail.com>
2023-12-09 21:32:45 +0100

    Logovanje commit git bash

---
+++ Common/Alarm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Common
+{
+    //Proveriti da li radi
+    public enum CriticalLevel { INFORMATION, WARNING, CRITICAL }
+
+    [DataContract]
+    public class Alarm
+    {
+        DateTime dateTime;
+        string name;
+        CriticalLevel level;
+
+        [DataMember]
+        public DateTime DateTime
+        {
+            get { return dateTime; }
+            set { dateTime = value; }
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public CriticalLevel Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
+
+        public Alarm(DateTime dateTime, string name, CriticalLevel level)
+        {
+            this.dateTime = dateTime;
+            this.name = name;
+            this.level = level;
+        }
+
+        public Alarm()
+        {
+        }
+
+        public override string ToString()
+        {
+            return dateTime.ToString() + " " + name + " " + level.ToString();
+        }
+    }
+}
---
+++ IDS/Program.cs
@@ -0,0 +1,73 @@
+﻿using Common;
+using Manager;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.ServiceModel;
+using Formatter = Manager.Formatter;
+
+namespace IDS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            string srvCertCN = "wcfservice";
+
+            X509Certificate2 srvCert = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople, StoreLocation.LocalMachine, srvCertCN);
+            EndpointAddress address = new EndpointAddress(new Uri("net.tcp://localhost:4001/IMalwareScanning"),
+                                      new X509CertificateEndpointIdentity(srvCert));
+
+            ChannelFactory<IMalwareScanning> channel = new ChannelFactory<IMalwareScanning>(binding, address);
+
+            string cltCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name.ToLower());
+
+            channel.Credentials.ServiceCertificate.Authentication.CertificateValidationMode = System.ServiceModel.Security.X509CertificateValidationMode.ChainTrust;
+            channel.Credentials.ServiceCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            /// Set appropriate client's certificate on the channel. Use CertManager class to obtain the certificate based on the "cltCertCN"
+            channel.Credentials.ClientCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, cltCertCN);
+
+            Console.WriteLine(WindowsIdentity.GetCurrent().Name);
+
+            /// Use CertManager class to obtain the certificate based on the "srvCertCN" representing the expected service identity.
+
+            IMalwareScanning proxy = channel.CreateChannel();
+
+            X509Certificate2 certificateSign = CertManager.GetCertificateFromStorage(StoreName.My,
+                   StoreLocation.LocalMachine, "wcfclient_sign");
+
+            string message = "Testiranje slanja poruke digitalnim potpisom.";
+
+            byte[] signature = DigitalSignature.Create(message, HashAlgorithm.SHA1, certificateSign);
+
+            try
+            {
+                proxy.SendMessage(message, signature);
+                Console.WriteLine("SendMessage() using {0} certificate finished. Press <enter> to continue ...", cltCertCN);
+                Console.WriteLine(proxy.SendAlarmToIDS());
+                proxy.SendAlarmToMS();
+                Audit.AuthenticationSuccess(Formatter.ParseName(WindowsIdentity.GetCurrent().Name));
+            }
+            catch
+            {
+                Console.WriteLine("Error: Authentification error. Close and try to connect again.");
+            }
+            
+
+            /*string wrongCertCN = "wrong_sign";
+            X509Certificate2 certificateSignWrong = CertManager.GetCertificateFromStorage(StoreName.My,
+                       StoreLocation.LocalMachine, wrongCertCN);
+
+            byte[] signatureWrong = DigitalSignature.Create(message, HashAlgorithm.SHA1, certificateSignWrong);
+
+            proxy.SendMessage(message, signatureWrong);
+            Console.WriteLine("SendMessage() using {0} certificate finished. Press <enter> to continue ...", wrongCertCN); */
+
+            Console.ReadKey();
+        }
+    }
+}
