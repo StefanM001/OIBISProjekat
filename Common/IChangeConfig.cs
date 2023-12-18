c27740b683fd69a342a34a67fc749b5203748686
Pinki223 <116367165+Pinki223@users.noreply.github.com>
2023-12-18 14:12:32 +0100

    radi skeniranje

---
+++ Common/IChangeConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IChangeConfig
+    {
+        [OperationContract]
+        void ReadConfiguration();
+
+        [OperationContract]
+        void AddProcess();
+
+        [OperationContract]
+        void ModifyProcess();
+
+        [OperationContract]
+        void DeleteProcess();
+
+        [OperationContract]
+        void DeleteConfigurationFile();
+    }
+}
