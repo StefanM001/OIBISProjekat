ad32964ae7ab546efa62e048d50085033067c64c
Lundjo <lukadjelic529@gmail.com>
2023-12-19 19:51:57 +0100

    komunikacija sa klijentom

--- Client/Program.cs
+++ Client/Program.cs
@@ -33,12 +33,49 @@ namespace Client
                 Console.WriteLine("3: Izmeni parametre konfiguracije");
                 Console.WriteLine("4: Izbrisi proces");
                 Console.WriteLine("5: Izbrisi konfiguracioni fajl");
+                Console.WriteLine("6: Zavrsi rad programa");
                 int izbor;
 
                 do
                 {
                     izbor = int.Parse(Console.ReadLine());
-                } while (izbor < 1 || izbor > 4);
+                } while (izbor < 1 || izbor > 6);
+
+                if(izbor == 1)
+                {
+                    foreach(string s in proxy.ReadConfiguration())
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+                else if(izbor == 2)
+                {
+                    Console.Write("Unesi proces: ");
+                    string proces = Console.ReadLine();
+                    Console.WriteLine(proxy.AddProcess(proces));
+                }
+                else if(izbor == 3)
+                {
+                    Console.Write("Uneti novi proces: ");
+                    string p1 = Console.ReadLine();
+                    Console.Write("Uneti proces za izmenu: ");
+                    string p2 = Console.ReadLine();
+                    Console.WriteLine(proxy.ModifyProcess(p1, p2));
+                }
+                else if(izbor == 4)
+                {
+                    Console.Write("Unesi proces: ");
+                    string proces = Console.ReadLine();
+                    Console.WriteLine(proxy.DeleteProcess(proces));
+                }
+                else if(izbor == 5)
+                {
+                    Console.WriteLine(proxy.DeleteConfigurationFile());
+                }
+                else
+                {
+                    break;
+                }
             }
 
             Console.ReadKey();
