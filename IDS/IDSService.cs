6cf3ef7055da9a2ab16bf19ff4402b6dc6c612bb
Pinki223 <116367165+Pinki223@users.noreply.github.com>
2023-12-22 19:30:00 +0100

    Add files via upload

---
+++ IDS/IDSService.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDS
+{
+    public class IDSService : IIDS
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
