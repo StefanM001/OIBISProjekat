7216ec7bcb8d16f80f31abd14365fe3038c7d7cf
Stefan Malinović <malina.stefan39@gmail.com>
2023-12-22 21:16:53 +0100

    New connection with signature

---
+++ Common/IChangeConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IChangeConfig
+    {
+        [OperationContract]
+        List<string> ReadConfiguration();
+
+        [OperationContract]
+        string AddProcess(string process);
+
+        [OperationContract]
+        string ModifyProcess(string p1, string p2);
+
+        [OperationContract]
+        string DeleteProcess(string process);
+
+        [OperationContract]
+        string DeleteConfigurationFile();
+    }
+}
---
+++ IDS/IDSService.cs
@@ -0,0 +1,41 @@
+﻿using Common;
+using Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDS
+{
+    public class IDSService : IIDS
+    {
+        public void SendAlarmToIDS(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void SendMessage(string message, byte[] sign)
+        {
+            //kad je u pitanju autentifikacija putem Sertifikata
+            string clientName = Formatter.ParseName(ServiceSecurityContext.Current.PrimaryIdentity.Name);
+
+            string clientNameSign = clientName + "_sign";
+            X509Certificate2 certificate = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople,
+                StoreLocation.LocalMachine, clientNameSign);
+
+            /// Verify signature using SHA1 hash algorithm
+            if (DigitalSignature.Verify(message, HashAlgorithm.SHA1, sign, certificate))
+            {
+                Console.WriteLine("Sign is valid");
+                SendAlarmToIDS(message);
+            }
+            else
+            {
+                Console.WriteLine("Sign is invalid");
+            }
+        }
+    }
+}
